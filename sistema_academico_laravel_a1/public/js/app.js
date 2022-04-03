450641cc8e40c166e7155a3606ae1cb63649b524
Duma Roberto Zelaya Mejia <usis007420@ugb.edu.sv>
2022-04-03 14:34:16 -0600

    ca

--- sistema_academico_laravel_a1/public/js/app.js
+++ sistema_academico_laravel_a1/public/js/app.js
@@ -51407,8 +51407,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Usuario\Desktop\aqui tarea\Programacion-IV-2022\sistema_academico_laravel_a1\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Usuario\Desktop\aqui tarea\Programacion-IV-2022\sistema_academico_laravel_a1\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Usuario\Desktop\laravel 2 ptm\PROGRAMACION-IV\sistema_academico_laravel_a1\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Usuario\Desktop\laravel 2 ptm\PROGRAMACION-IV\sistema_academico_laravel_a1\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
