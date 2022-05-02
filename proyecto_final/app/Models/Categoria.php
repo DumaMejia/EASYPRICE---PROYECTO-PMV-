2ce9e8ab390d358f695568fd6eaeb92158978961
Duma Roberto Zelaya Mejia <usis007420@ugb.edu.sv>
2022-05-01 21:26:53 -0600

    d

--- proyecto_final/app/Models/Categoria.php
+++ proyecto_final/app/Models/Categoria.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
-    protected $fillable = ['idCategoria', 'codigo', 'nombre'];
+    protected $fillable = ['id','idCategoria', 'codigo', 'nombre'];
 }
